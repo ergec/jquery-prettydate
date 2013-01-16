d480e8165af381b7b4c6f00c4174a5e718c46a44
ergec <ergecsenturk@gmail.com>
2013-01-16 15:44:52 +0200

    Improved translation for Turkish language
    * Added translation for days after functionality in Turkish language
    * Added translation months and years in Turkish language for default functionality

--- i18n/jquery.prettydate-tr.js
+++ i18n/jquery.prettydate-tr.js
@@ -1,5 +1,13 @@
 /* Turkish */
 $.extend($.prettyDate.messages, {
+	tomorrow: 'Yarın',
+	daysafter: $.prettyDate.template('{0} gün sonra'),
+	weekafter: '1 hafta sonra',
+	weeksafter: $.prettyDate.template('{0} hafta sonra'),
+	monthafter: '1 ay sonra',
+	monthsafter: $.prettyDate.template('{0} ay sonra'),
+	yearafter: '1 yıl sonra',
+	yearsafter: $.prettyDate.template('{0} yıl sonra'),
 	now: 'Az önce',
 	minute: '1 dakika önce',
 	minutes: $.prettyDate.template('{0} dakika önce'),
@@ -7,5 +15,9 @@ $.extend($.prettyDate.messages, {
 	hours: $.prettyDate.template('{0} saat önce'),
 	yesterday: 'Dün',
 	days: $.prettyDate.template('{0} gün önce'),
-	weeks: $.prettyDate.template('{0} hafta önce')
+	weeks: $.prettyDate.template('{0} hafta önce'),
+	month: '1 ay önce',
+	months: $.prettyDate.template('{0} ay önce'),
+	year: '1 yıl önce',
+	years: $.prettyDate.template('{0} yıl önce')
 });
